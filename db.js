cf940a9eacd4fa6301f8152b1c6a15ca180e1568
jj4020 <104187160+jj4020@users.noreply.github.com>
2024-12-20 13:39:44 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql');
+
+function getConnection(){
+    var con = mysql.createConnection({
+        host:"localhost",
+        user:"root",
+        password:"",
+        database:"ambrosia"
+    });
+    console.log("connected!");
+   
+   
+    return con;
+}
+
+module.exports.getConnection = getConnection;
+#hello
